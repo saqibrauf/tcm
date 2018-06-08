7144c86d0840525d1ae3fc7759c04aa8e253135e
saqib <saqibrauf@yahoo.com>
2018-06-08 15:11:35 +0500

    Structure Changed

--- match/views.py
+++ match/views.py
@@ -9,9 +9,8 @@ def index(request):
 	todays_matches = Match.objects.all().filter(date__day=now.day, date__month=now.month, date__year=now.year).order_by('date','time')
 	upcoming_matches = Match.objects.all().filter(date__gt=now).order_by('date')
 	recent_matches = Match.objects.all().filter(date__lt=now).order_by('-date')
-	series = Series.objects.order_by('-id')[0]
-	all_series = Series.objects.all().order_by('-date')[1:5]
-	return render(request, 'match/index.html', context={'todays_matches':todays_matches, 'upcoming_matches':upcoming_matches, 'recent_matches':recent_matches, 'series': series, 'all_series':all_series})
+	all_series = Series.objects.all().order_by('-date')[:5]
+	return render(request, 'match/index.html', context={'todays_matches':todays_matches, 'upcoming_matches':upcoming_matches, 'recent_matches':recent_matches, 'all_series':all_series})
 
 
 def match_detail(request, slug, t_slug=''):
