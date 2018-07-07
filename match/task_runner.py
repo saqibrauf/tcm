3f386f0c034427986d59de9f8d31fec154f93046
saqibrauf <saqibrauf@yahoo.com>
2018-07-07 22:32:42 +0500

    Scrip edited

--- match/task_runner.py
+++ match/task_runner.py
@@ -1,11 +1,11 @@
 import sched, time
-import match.scores
+from . import scores
 
 s = sched.scheduler(time.time, time.sleep)
 
 def run_task(sc): 
     print ("Doing stuff...")
-    match.scores.store_data()
+    scores.store_data()
     s.enter(10, 1, run_task, (sc,))
 
 s.enter(10, 1, run_task, (s,))
