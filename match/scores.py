5c7112df8c6606ace24689ee9183c87ce0d381a8
saqibrauf <saqibrauf@yahoo.com>
2018-07-08 19:37:07 +0500

    scrip error removed

--- match/scores.py
+++ match/scores.py
@@ -20,8 +20,8 @@ class ScoreBoard():
 		data = soup.find(class_= 'cscore_competitors')
 
 		#Foot Notes
-		comm = soup.find_all(class_='cscore_notes_game')
-		comm = comm[0].get_text()
+		comm = soup.find(class_='cscore_notes_game')
+		comm = comm.get_text()
 
 		#CHILD DIVS
 		teams = data.find_all(class_='cscore_name--long')
