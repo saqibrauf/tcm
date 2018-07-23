b1a57328b3cebe8f20d0ce0cc31ae686772ea0e2
saqibrauf <saqibrauf@yahoo.com>
2018-07-24 01:02:36 +0500

    258963

--- match/views.py
+++ match/views.py
@@ -29,41 +29,103 @@ def index(request):
 
 
 def match_detail(request, slug):
+	now = timezone.localtime(timezone.now())
 	try:
 		match = Match.objects.get(slug=slug)
 		series = Series.objects.get(pk=match.series.id)
-		schedule = series.match_set.all().order_by('date')
+		schedule = series.match_set.filter(date__gt=now).order_by('date')
+		if not schedule:
+			schedule = series.match_set.filter(date__lt=now).order_by('-date')
+		recent_matches = Match.objects.filter(date__lt=now).exclude(date__day=now.day, date__month=now.month, date__year=now.year).order_by('-date')
+		latest_series = Series.objects.order_by('-date')
+		tag_cloud = Tag.objects.all()
 		messages = match.message_set.all()
-		return render(request, 'match/match_detail.html', context={'series':series, 'schedule':schedule, 'match':match, 'messages':messages})
+
+		context={
+			'series':series,
+			'schedule' : schedule,
+			'match':match,
+			'messages':messages,
+			'recent_matches' : recent_matches,
+			'latest_series' : latest_series,
+			'tag_cloud' : tag_cloud,
+		}
+
+		return render(request, 'match/match_detail.html', context)
+
 	except Match.DoesNotExist:
 		return render(request, 'match/404.html')
 
 
 def upcoming_matches(request):
 	now = timezone.localtime(timezone.now())
+	recent_matches = Match.objects.filter(date__lt=now).order_by('-date')
 	upcoming_matches = Match.objects.filter(date__gt=now).order_by('date')
+	latest_series = Series.objects.order_by('-date')
+	tag_cloud = Tag.objects.all()
 
-	return render(request, 'match/upcoming_matches.html', context={'upcoming_matches' : upcoming_matches})
+	context = {
+		'recent_matches' : recent_matches,
+		'upcoming_matches' : upcoming_matches,
+		'latest_series' : latest_series,
+		'tag_cloud' : tag_cloud,
+	}
+
+	return render(request, 'match/upcoming_matches.html', context)
 
 def recent_matches(request):
 	now = timezone.localtime(timezone.now())
 	recent_matches = Match.objects.filter(date__lt=now).order_by('-date')
+	upcoming_matches = Match.objects.filter(date__gt=now).order_by('date')
+	latest_series = Series.objects.order_by('-date')
+	tag_cloud = Tag.objects.all()
 
-	return render(request, 'match/recent_matches.html', context={'recent_matches' : recent_matches})
+	context = {
+		'recent_matches' : recent_matches,
+		'upcoming_matches' : upcoming_matches,
+		'latest_series' : latest_series,
+		'tag_cloud' : tag_cloud,
+	}
+
+	return render(request, 'match/recent_matches.html', context)
 
 
 def series_detail(request, slug):
+	now = timezone.localtime(timezone.now())
 	series = Series.objects.get(series_slug=slug)
 	schedule = series.match_set.all().order_by('date')
+	recent_matches = Match.objects.filter(date__lt=now).exclude(date__day=now.day, date__month=now.month, date__year=now.year).order_by('-date')
+	latest_series = Series.objects.order_by('-date')
+	tag_cloud = Tag.objects.all()
 
-	return render(request, 'match/series_detail.html', context={'series' : series, 'schedule' : schedule})
+	context={
+		'series': series,
+		'schedule': schedule,
+		'recent_matches' : recent_matches,
+		'latest_series' : latest_series,
+		'tag_cloud' : tag_cloud,
+	}
+
+	return render(request, 'match/series_detail.html', context)
 
 
 def tag_detail(request, slug):
+	now = timezone.localtime(timezone.now())
 	tag = Tag.objects.get(slug=slug)
 	tag_archive = Tag.objects.get(slug=slug).match_set.all()
+	recent_matches = Match.objects.filter(date__lt=now).exclude(date__day=now.day, date__month=now.month, date__year=now.year).order_by('-date')
+	latest_series = Series.objects.order_by('-date')
+	tag_cloud = Tag.objects.all()
 
-	return render(request, 'match/tag.html', context={'tag' : tag, 'tag_archive' : tag_archive})
+	context={
+		'tag': tag,
+		'tag_archive': tag_archive,
+		'recent_matches' : recent_matches,
+		'latest_series' : latest_series,
+		'tag_cloud' : tag_cloud,
+	}
+
+	return render(request, 'match/tag.html', context)
 
 
 
